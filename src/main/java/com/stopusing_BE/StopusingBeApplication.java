d41448233307cad8b179a4604805759e567fdfe6
NamMinwu <minwoo000625@naver.com>
2025-08-29 03:05:44 +0900

    Initial commit

---
+++ src/main/java/com/stopusing_BE/StopusingBeApplication.java
@@ -0,0 +1,13 @@
+package com.stopusing_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopusingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopusingBeApplication.class, args);
+	}
+
+}
