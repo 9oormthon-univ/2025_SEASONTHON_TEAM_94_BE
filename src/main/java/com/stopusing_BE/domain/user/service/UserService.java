36e7568b401767718b9cdbbccee248a30f13a64b
NamMinwu <minwoo000625@naver.com>
2025-08-31 16:57:45 +0900

    FEAT : user - transaction relation 추가

---
+++ src/main/java/com/stopusing_BE/domain/user/service/UserService.java
@@ -0,0 +1,21 @@
+package com.stopusing_BE.domain.user.service;
+
+import com.stopusing_BE.domain.user.entity.User;
+import com.stopusing_BE.domain.user.repository.UserRepository;
+import com.stopusing_BE.global.common.exception.CustomException;
+import com.stopusing_BE.global.common.exception.code.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+  private final UserRepository userRepository;
+
+  @Transactional(readOnly = true)
+  public User getByIdOrThrow(Long userId) {
+    return userRepository.findById(userId)
+        .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+  }
+}
